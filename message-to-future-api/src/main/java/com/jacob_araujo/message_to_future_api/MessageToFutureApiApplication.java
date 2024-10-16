c48b4d31d1f28dc6179d3f9e644dd0e48da70cab
Jacob Alexandre <49577844+JacobAraujo@users.noreply.github.com>
2024-10-16 20:52:00 -0300

    feat: inicialize api

---
+++ message-to-future-api/src/main/java/com/jacob_araujo/message_to_future_api/MessageToFutureApiApplication.java
@@ -0,0 +1,13 @@
+package com.jacob_araujo.message_to_future_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageToFutureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageToFutureApiApplication.class, args);
+	}
+
+}
---
+++ message-to-future-api/src/test/java/com/jacob_araujo/message_to_future_api/MessageToFutureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob_araujo.message_to_future_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageToFutureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
