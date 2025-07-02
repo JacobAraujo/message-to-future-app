48f117d564b7721234b6e6adc4b7930d326226d4
Jacob Alexandre <49577844+JacobAraujo@users.noreply.github.com>
2025-07-01 22:42:14 -0300

    fix(Header): remove username of wellcome

--- message-to-future-front/src/components/Header.jsx
+++ message-to-future-front/src/components/Header.jsx
@@ -6,17 +6,17 @@ function Header() {
   const { user, logout } = useAuth();
 
   return (
-    <header className="bg-blue-600 text-white p-4 rounded-b-lg shadow-md flex items-center justify-between">
+    <header className="sticky top-0 z-30 flex items-center justify-between w-full px-4 py-2 bg-[#1500B3]/80 dark:bg-slate-900/80 shadow-md text-white">
       <div className="flex items-center space-x-2">
-        <FiUser className="text-2xl" /> {/* Ícone de usuário */}
-        <p className="text-lg font-medium">Bem-vindo, {user?.name || 'Usuário'}</p>
+        <FiUser className="text-2xl" />
+        <span>Bem-vindo</span>
       </div>
       <button
         onClick={logout}
         className="flex items-center bg-white text-blue-600 font-semibold py-2 px-4 rounded-lg hover:bg-blue-50 transition-colors shadow-md"
-        title="Sair da conta" // Tooltip ao passar o mouse
+        title="Sair da conta"
       >
-        <FiLogOut className="mr-2" /> {/* Ícone de logout */}
+        <FiLogOut className="mr-2" />
         Sair
       </button>
     </header>
